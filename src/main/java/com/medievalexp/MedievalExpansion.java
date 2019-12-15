f2fe879f8ba3232d5d3b1a4058faba96505de292
ProfHaxx <roman.shchekotov@mail.ru>
2019-12-15 10:17:43 +0100

    Steel Armor Update

--- src/main/java/com/medievalexp/MedievalExpansion.java
+++ src/main/java/com/medievalexp/MedievalExpansion.java
@@ -1,12 +1,14 @@
 package com.medievalexp;
 
 import com.medievalexp.groups.MedievalExpansionGeneralGroup;
+import com.medievalexp.indices.ArmorMaterialIndex;
 import com.medievalexp.indices.BlockIndex;
 import com.medievalexp.indices.ItemIndex;
 import com.medievalexp.indices.ToolMaterialIndex;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
@@ -19,13 +21,15 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Objects;
+
 @Mod("medexp")
 public class MedievalExpansion {
-    public static MedievalExpansion instance;
+    private static MedievalExpansion instance;
     public static final String modid = "medexp";
     private static final Logger logger = LogManager.getLogger(modid);
 
-    public static final ItemGroup general = new MedievalExpansionGeneralGroup();
+    private static final ItemGroup general = new MedievalExpansionGeneralGroup();
 
     public MedievalExpansion() {
         instance = this;
@@ -59,7 +63,12 @@ public class MedievalExpansion {
                     ItemIndex.steel_hoe = new HoeItem(ToolMaterialIndex.STEEL, 1.0f, new Item.Properties().group(general)).setRegistryName("steel_hoe"),
                     ItemIndex.steel_shovel = new ShovelItem(ToolMaterialIndex.STEEL, -3.0f, 6.0f, new Item.Properties().group(general)).setRegistryName("steel_shovel"),
 
-                    ItemIndex.steel_block = new BlockItem(BlockIndex.steel_block, new Item.Properties().group(general)).setRegistryName(BlockIndex.steel_block.getRegistryName())
+                    ItemIndex.steel_helmet = new ArmorItem(ArmorMaterialIndex.STEEL, EquipmentSlotType.HEAD, new Item.Properties().group(general)).setRegistryName("steel_helmet"),
+                    ItemIndex.steel_chestplate = new ArmorItem(ArmorMaterialIndex.STEEL, EquipmentSlotType.CHEST, new Item.Properties().group(general)).setRegistryName("steel_chestplate"),
+                    ItemIndex.steel_leggings = new ArmorItem(ArmorMaterialIndex.STEEL, EquipmentSlotType.LEGS, new Item.Properties().group(general)).setRegistryName("steel_leggings"),
+                    ItemIndex.steel_boots = new ArmorItem(ArmorMaterialIndex.STEEL, EquipmentSlotType.FEET, new Item.Properties().group(general)).setRegistryName("steel_boots"),
+
+                    ItemIndex.steel_block = new BlockItem(BlockIndex.steel_block, new Item.Properties().group(general)).setRegistryName(Objects.requireNonNull(BlockIndex.steel_block.getRegistryName()))
             );
             logger.info("Items registered!");
         }
